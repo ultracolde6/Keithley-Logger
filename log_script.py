71ead41c4b5e80d0d7c33a6cc246998e585bea7a
E6 <ultracolde6@gmail.com>
2020-06-28 21:26:30 -0700

    had to fix log_script

--- log_script.py
+++ log_script.py
@@ -96,7 +96,7 @@ def main():
 
     keithley_device = logger.Keithley(port=keithley_port, timeout=15, quiet=True)
     save_groups = [mag_group, ion_pump_group, ion_gauge_group]
-    logger.Logger(save_groups=save_groups, device=keithley_device, log_freq=t_read_freq, quiet=False)
+    logger_object = logger.Logger(save_groups=save_groups, device=keithley_device, log_freq=t_read_freq, quiet=False)
 
     plotters = [mag_plotter, ion_pump_plotter, ion_gauge_plotter]
     plotter_manager = PlotterManagerWindow(plotters)
